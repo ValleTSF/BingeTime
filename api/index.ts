e45b932357ad8e2a951b707cf22a4e2f3c1a06b5
ValleTSF <Valletsf@gmail.com>
2023-12-17 18:33:14 +0100

    refactor api

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,8 @@
 const baseUri = "https://api.tvmaze.com/";
 
 export const getEpisodesSingleSearch = (input: string) => {
-  const singleSearchUri = "singlesearch/shows?q=";
-  const endUri = "&embed=episodes";
-  const query = baseUri + singleSearchUri + input + endUri;
-
-  return fetch(query)
-    .then((response) => response.json())
-    .then((json) => json)
-    .catch((error) => console.log("error", error));
-};
-
-export const getEpisodeInformation = (id: string) => {
-  const episodeUri = `episodes/${id}`;
-  const query = baseUri + episodeUri;
+  const singleSearchUri = `singlesearch/shows?q=${input}/&embed=episodes`;
+  const query = baseUri + singleSearchUri;
 
   return fetch(query)
     .then((response) => response.json())
