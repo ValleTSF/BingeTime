5564064eb03432b83ebb592be49e09feee4cf93f
ValleTSF <Valletsf@gmail.com>
2023-12-17 18:09:25 +0100

    removed unsued styling

--- screens/HomeScreen/styled.ts
+++ screens/HomeScreen/styled.ts
@@ -1,6 +1,4 @@
-import Icon from "react-native-vector-icons/FontAwesome5";
 import styled from "styled-components/native";
-import { TextInput } from "react-native";
 
 export const Header = styled.Text`
   font-size: 52px;
@@ -16,35 +14,4 @@ export const Container = styled.ImageBackground`
   align-items: center;
   justify-content: flex-start;
   background-color: #383838;
-`;
-
-export const SearchContainer = styled.View`
-  margin-top: 40px;
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const SearchBar = styled.TouchableOpacity`
-  display: flex;
-  background-color: #5e5e5e;
-  padding: 5px;
-  width: 60%;
-  border-radius: 10px;
-  height: 30px;
-  flex-direction: row;
-  justify-content: center;
-`;
-
-export const SearchInput = styled(TextInput)`
-  color: #dbdbdb;
-  width: 95%;
-`;
-
-export const BackButton = styled(Icon)`
-  font-size: 20px;
-  margin-right: 10px;
-  color: #dbdbdb;
 `;
